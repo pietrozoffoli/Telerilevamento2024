dd1f08a50d016fc321a8c92fe0a96cfd2a53f581
pietrozoffoli <162345159+pietrozoffoli@users.noreply.github.com>
2024-03-14 17:43:13 +0100

    Update 03_visualisation.R

--- 03_visualisation.R
+++ 03_visualisation.R
@@ -3,9 +3,79 @@
 library(terra)
 library(imageRy)
 
-# all fuctions begin with 'im.'
+# all functions begin with 'im.'
 
 im.list() # lists all the data loaded in the package imageRy
 
 # im.import()  imports data from the list 
-mato <- im.import("matogrosso_ast_2006209_lrg.jpg") #then I assigned it to the object 'mato'
+mato <- im.import("matogrosso_ast_2006209_lrg.jpg") # I assigned it to the object 'mato'
+                                                    # Image comes from Earth Observatory by NASA
+                                                    # https://earthobservatory.nasa.gov/ you may use this website to gather images for your project
+                                                    
+# plotting the data
+plot(mato)
+
+#importing another image to see different bands
+b2 <- im.import("sentinel.dolomites.b2.tif")    # sentinel-2's band 2 corresponds to the wavelength of the colour blue
+                                                # this place is Le Tofane
+
+# changing colour palette
+clg <- colorRampPalette(c("black", "grey", "white"))(100) # the number outside the main argument defines how many shades will be shown
+plot(b2, col=clg)                                         # you can use as many colours as you want
+
+# changing colour palette again
+clcyan <- colorRampPalette(c("magenta", "cyan", "yellow"))(300) # the number outside the main argument defines how many shades will be shown
+plot(b2, col=clcyan)
+
+
+# changing colour palette yet again
+clch <- colorRampPalette(c("magenta", "cyan", "chartreuse", "yellow"))(300) # the number outside the main argument defines how many shades will be shown
+plot(b2, col=clch)
+
+# importing additional bands
+# import the green band from Sentinel-2 (band 3) (560 nm)
+b3 <- im.import("sentinel.dolomites.b3.tif")
+plot(b3, col=clch)
+
+# import the red band from Sentinel-2 (band 4) (665 nm)
+b4 <- im.import("sentinel.dolomites.b4.tif")
+plot(b4, col=clch)
+
+# import the NIR band from Sentinel-2 (band 8) (842)
+b8 <- im.import("sentinel.dolomites.b8.tif")
+plot(b8, col=clch)
+
+# creating a multiframe. It works like a matrix (rows and columns), this one woulb be 2x2 matrix
+
+par(mfrow=c(2,2)) 
+plot(b2, col=clch)
+plot(b3, col=clch)
+plot(b4, col=clch)
+plot(b8, col=clch)
+
+# another way to get the same result but a bit better
+
+stacksent <- c(b2, b3, b4, b8)
+plot(stacksent, col=clch)
+
+# Exercise: plot the four bands in a single raw
+
+par(mfrow=c(1,4)) 
+plot(b2, col=clch)
+plot(b3, col=clch)
+plot(b4, col=clch)
+plot(b8, col=clch)
+
+# overlapping the four bands creating a stack (making of a satellite image)
+# it uses metadata to gice names to the plots using the names of the images
+
+stacksent<-c(b2, b3, b4, b8)
+plot(stacksent, col=clch)
+
+dev.off() #deletes the previous device
+plot(stacksent[[4]], col=clch) # I'm using two square parenthesis because stacksent is 2-dimensional object, if it were an array I would have used one set of parenthesis
+
+
+
+
+
