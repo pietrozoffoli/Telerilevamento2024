6b6a925ca242fbb8df5df93db467e1cf0629f4be
Pietro Zoffoli <162345159+pietrozoffoli@users.noreply.github.com>
2024-04-11 17:35:55 +0200

    Update 06_time_series.R

--- 06_time_series.R
+++ 06_time_series.R
@@ -24,6 +24,43 @@ plot(difEN,col=cl)
 
 # measuring the ammopunt of lost ice mass in Greenlan
 
+# we are using temperature as a proxy to quantify the ice melt
+clg<-colorRampPalette(c("black","blue","white","red"))(100)
+
+g2000<-im.import("greenland.2000.tif")
+g2005<-im.import("greenland.2005.tif")
+g2010<-im.import("greenland.2010.tif")
+g2015<-im.import("greenland.2015.tif")
+
+par(mfrow=c(2,2))
+plot(g2000,col=clg)
+plot(g2005,col=clg)
+plot(g2010,col=clg)
+plot(g2015,col=clg)
+
+dev.off()
+
+par(mfrow=c(1,2))
+plot(g2000,col=clg)
+plot(g2015,col=clg)
+
+# stacking
+
+greenland<-c(g2000,g2005,g2010,g2015)
+plot(greenland,col=clg)
+
+dev.off()
+# quantifying the difference
+clgreen<-colorRampPalette(c("red","white","blue"))(100)
+difg=greenland[[1]]-greenland[[4]]  # 2000-2005 
+plot(difg,col=clgreen)                   # blue=higer Temperature; red=lower Temperature (melting)
+
+
+# this is to show in which year there is a higer temperature in a certain area
+im.plotRGB(greenland, r=1, g=2, b=4) # g2000 on red, g2005 on green, g2015 on blue
+
+
+
 
 
 
